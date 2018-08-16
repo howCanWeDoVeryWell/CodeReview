7d27d2dab7ea17e41511d67acf0d34ac05c32351
jieunjung <sirupe@nate.com>
2018-08-16 15:28:21 +0900

    codekata 1-1. Multiples 클래스 multipleSet 변수 추가

--- eunjung/src/com/java/Multiples.java
+++ eunjung/src/com/java/Multiples.java
@@ -1,13 +1,28 @@
 package com.java;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Multiples {
     private final int maxNumber;
+    private final int baseNumber;
+    private List<Integer> multipleSet;
     
-    public Multiples(int maxNumber) {
+    public Multiples(int maxNumber, int baseNumber) {
         this.maxNumber = maxNumber;
+        this.baseNumber = baseNumber;
+        this.multipleSet = Arrays.asList(3, 6, 9);
     }
     
     public int getMaxNumber() {
         return maxNumber;
+    }
+    
+    public int getBaseNumber() {
+        return baseNumber;
+    }
+    
+    public List<Integer> getMultipleSet() {
+        return this.multipleSet;
     }
 }
--- eunjung/test/com/java/CommonMultiplesTest.java
+++ eunjung/test/com/java/CommonMultiplesTest.java
@@ -2,13 +2,26 @@ package com.java;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CommonMultiplesTest {
-    private final int maxNumber = 10;
+    private int maxNumber = 10;
+    private int baseNumber = 3;
+    private List<Integer> resultMultiples = Arrays.asList(3, 6, 9); 
+    
     @Test
     public void testNewMultiple() {
-        Multiples multiples = new Multiples(maxNumber);
+        Multiples multiples = new Multiples(maxNumber, baseNumber);
         assertEquals(multiples.getMaxNumber(), maxNumber);
+        assertEquals(multiples.getBaseNumber(), baseNumber);
+    }
+    
+    @Test
+    public void testMultiples() {
+        Multiples multiples = new Multiples(maxNumber, baseNumber);
+        assertArrayEquals(resultMultiples.toArray(), multiples.getMultipleSet().toArray());
     }
 }
