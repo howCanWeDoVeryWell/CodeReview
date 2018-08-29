c38f3a358f89a56fede4efd4cbc4efb22399e7ce
dolen <dolen.sirupe@kakaocorp.com>
2018-08-29 11:31:30 +0900

    페어 프로그래밍 작성.

---
+++ dolen/src/operator/constants/Common.java
@@ -0,0 +1,6 @@
+package operator.constants;
+
+public abstract class Common {
+    public final static int MAX_LOOP_COUNT = 100;
+    public final static String END_OF_PROGRAM = "프로그램을 종료합니다.";
+}
---
+++ dolen/src/operator/constants/Input.java
@@ -0,0 +1,6 @@
+package operator.constants;
+
+public abstract class Input {
+    public final static String CHOICES_ONE_OF_DIGIT = "숫자 1자리를 입력해 주세요.";
+    public final static String INPUT_TYPE_ERROR = "잘못 입력 하셨습니다.";
+}
