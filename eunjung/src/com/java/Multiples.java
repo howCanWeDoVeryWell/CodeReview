0da06f62e7f02b8a1a7a74a4a14b1bfe77aeb8f8
jieunjung <sirupe@nate.com>
2018-09-10 12:57:55 +0900

    1. CommonMultiplesTest.java -> MultipleTest.java : 클래스 이름 변경 2. MultiplesTest.java : 상수를 제거하고 지역변수로 변경. 3. Multiples.java    * 인스턴스 변수에 this. 붙임.    * getResultMultipleSet() -> getResultMultipleValues() 로 이름 변경    * getResultMultipleValues() 내부의 배수 집합 자료형을 `int []` 에서 `Set` 으로 변경

--- eunjung/src/com/java/Multiples.java
+++ eunjung/src/com/java/Multiples.java
@@ -1,55 +1,42 @@
 package com.java;
 
 import java.util.Arrays;
-import java.util.stream.IntStream;
+import java.util.HashSet;
+import java.util.Set;
+
 // 10 미만의 자연수에서 3과 5의 배수를 구하면 3, 5, 6, 9이다. 이들의 총합은 23이다. 1000 미만의 자연수에서 3, 5의 배수의 총합을 구하라.
 public class Multiples {
+    private final int START_MULTIPLE_NUMBER = 1;
     private int maxNumber;
     
     public Multiples(int maxNumber) {
         this.maxNumber = maxNumber;
     }
     
-    public int getMaxNumber() {
-        return maxNumber;
-    }
-    
-    public void setMaxNumber(int maxNumber) {
-        this.maxNumber = maxNumber;
-    }
-    
-    public int [] getResultMultipleSet(int startNumber) {
-        int setSize = getMultipleSetSize(startNumber);
-        int [] resultMultipleSet = new int[setSize];
+    public int getSumOfMultipleOfTwoNumbers(int firstNumber, int secondNumber) {
+        Set<Integer> multipleSet = getResultMultipleValues(firstNumber, secondNumber);
         
-        int multipleNumber = startNumber;
-        for (int i = 0; i < setSize; i++) {
-            resultMultipleSet[i] = (multipleNumber);
-            multipleNumber += startNumber;
-        }
+        return multipleSet.stream().reduce((a, b) -> a + b).get();
+    }
+    
+    public Set<Integer> getResultMultipleValues(int... numbers) {
+        Set<Integer> resultMultipleSet = new HashSet<>();
+        Arrays.stream(numbers).forEach(number -> {
+            if (number != 0) {
+                for (int i = number; i < maxNumber; i += number) {
+                    resultMultipleSet.add(i);
+                } 
+            }
+        });
         
         return resultMultipleSet;
     }
     
-    private int getMultipleSetSize(int startNumber) {
-        int setSize = 0;
-        if (startNumber > 0) {
-            setSize = maxNumber / startNumber;
-            if (maxNumber % setSize == 0) {
-                setSize--;
-            }
-        }
-        return setSize;
+    public int getMaxNumber() {
+        return this.maxNumber;
     }
     
-    public int getSumOfMultipleOfTwoNumbers(int firstNumber, int secondNumber) {
-        int [] firstNumberMultipleSet = getResultMultipleSet(firstNumber);
-        int [] secondNumberMultipleSet = getResultMultipleSet(secondNumber);
-    
-        int firstSumNumberOfMultiple = Arrays.stream(firstNumberMultipleSet).sum();
-        int secondSumnumberOfMultiple = Arrays.stream(secondNumberMultipleSet).filter(secondNum -> 
-            IntStream.of(firstNumberMultipleSet).noneMatch(firstNum -> firstNum == secondNum)
-        ).sum();
-        return firstSumNumberOfMultiple + secondSumnumberOfMultiple;
+    public void setMaxNumber(int maxNumber) {
+        this.maxNumber = maxNumber;
     }
 }
--- eunjung/test/com/java/PagingTest.java
+++ eunjung/test/com/java/PagingTest.java
@@ -6,11 +6,59 @@ import org.junit.jupiter.api.Test;
 public class PagingTest {
     @Test
     public void testGetTotalPageCount() {
+        // Given
         Paging paging = new Paging();
-        Assertions.assertEquals(0, paging.getTotalPageCount(0, 0));
-        Assertions.assertEquals(1, paging.getTotalPageCount(1, 1));
-        Assertions.assertEquals(2, paging.getTotalPageCount(2, 1));
-        Assertions.assertEquals(2, paging.getTotalPageCount(3, 2));
-        Assertions.assertEquals(4, paging.getTotalPageCount(10, 3));
+        int totalListCount = 0;
+        int countOfOnePage = 0;
+        
+        // When
+        int result = paging.getTotalPageCount(totalListCount, countOfOnePage);
+        
+        // Then
+        Assertions.assertEquals(0, result);
+        
+        
+        // Given
+        totalListCount = 1;
+        countOfOnePage = 1;
+        
+        // When
+        result = paging.getTotalPageCount(totalListCount, countOfOnePage);
+        
+        // Then
+        Assertions.assertEquals(1, result);
+    
+    
+        // Given
+        totalListCount = 2;
+        countOfOnePage = 1;
+    
+        // When
+        result = paging.getTotalPageCount(totalListCount, countOfOnePage);
+    
+        // Then
+        Assertions.assertEquals(2, result);
+    
+    
+        // Given
+        totalListCount = 3;
+        countOfOnePage = 2;
+    
+        // When
+        result = paging.getTotalPageCount(totalListCount, countOfOnePage);
+    
+        // Then
+        Assertions.assertEquals(2, result);
+    
+    
+        // Given
+        totalListCount = 10;
+        countOfOnePage = 3;
+    
+        // When
+        result = paging.getTotalPageCount(totalListCount, countOfOnePage);
+    
+        // Then
+        Assertions.assertEquals(4, result);
     }
 }
