383b1012bbda9b38ba2be208c3b49a0040a13b9e
dolen <dolen.sirupe@kakaocorp.com>
2018-09-28 16:26:52 +0900

    문구 변경

--- dolen/src/mvc/home/Application.java
+++ dolen/src/mvc/home/Application.java
@@ -7,7 +7,7 @@ import mvc.home.view.TopComponent;
 
 public class Application {
     public static void main(String[] args) {
-        Model model = new Model("initialize data but this data don't have to be String. The type of the object is much better.");
+        Model model = new Model("This is initialization data but this data don't have to be String. The type of the object is much better.");
         TopComponent topComponent = new TopComponent();
         MainComponent mainComponent = new MainComponent();
         new Controller(model, topComponent, mainComponent);
